02e2cd78cb609b69284cce424db10388dfb8e38f
Gideon Mutai <giddymutai13@gmail.com>
2024-04-10 18:03:49 +0300

    iniatial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+// listens for the submit event
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target['new-task-description'].value)
+  })
+
+})
+
+// function to create the todo list
+function createToDo(description){
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "remove"
+  let listItems = document.createElement('li');
+  listItems.textContent = description;
+  listItems.appendChild(btn)
+  document.querySelector('#tasks').appendChild(listItems)
+}
+
+// fucntion to handle deletion of the tasks
+function handleDelete(e){
+  e.target.parentNode.remove()
+ }
