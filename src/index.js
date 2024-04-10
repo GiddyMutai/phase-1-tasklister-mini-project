41234f5ade16e4440006631cde6d2deb0d08bce3
Gideon Mutai <giddymutai13@gmail.com>
2024-04-10 18:07:38 +0300

    iniatial commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 // listens for the submit event
 document.addEventListener("DOMContentLoaded", () => {
-
-  let form = document.querySelector('form')
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    createToDo(e.target['new-task-description'].value)
-  })
-
-})
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createToDo(e.target["new-task-description"].value);
+  });
+});
 
 // function to create the todo list
-function createToDo(description){
-  let btn = document.createElement('button')
-  btn.addEventListener('click', handleDelete)
-  btn.textContent = "remove"
-  let listItems = document.createElement('li');
+function createToDo(description) {
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "remove";
+  let listItems = document.createElement("li");
   listItems.textContent = description;
-  listItems.appendChild(btn)
-  document.querySelector('#tasks').appendChild(listItems)
+  listItems.appendChild(btn);
+  document.querySelector("#tasks").appendChild(listItems);
 }
 
 // fucntion to handle deletion of the tasks
-function handleDelete(e){
-  e.target.parentNode.remove()
- }
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
